51185ee25c8dbf97140cb9c599ff847c23cc6b96
idohomri <idohomri@outlook.co.il>
2022-06-15 13:24:06 +0300

    .

--- exercises_w3resources.py
+++ exercises_w3resources.py
@@ -1,5 +1,6 @@
 # this exercises are taken from https://www.w3resource.com/python-exercises/python-basic-exercises.php
 
+# calendar
 def ex12():
     import calendar
 
@@ -18,6 +19,14 @@ def ex12():
             print('Month or Year are in incorrect format, try again..')
         print('\n')
 
+# check if n exists in group_n
+def ex25(group_n, n):
+    for x in group_n:
+        if(x == n):
+            return True 
+    return False
+
+# even and braker
 def ex28():
     numbers = [    
         386, 462, 47, 418, 907, 344, 236, 375, 823, 566, 597, 978, 328, 615, 953, 345, 
@@ -35,10 +44,30 @@ def ex28():
             print(str(n) + " - the braker")
             break
 
+# array comperation
 def ex29():
     color_list_1 = set(["White", "Black", "Red"])
     color_list_2 = set(["Red", "Green"])
 
     print(color_list_1.difference(color_list_2))
     print(color_list_2.difference(color_list_1))
+
+# triangle area
+def ex30(h, w):
+    return float((h * w) / 2)
+
+# check if file exsists
+def ex42(file):
+    import os.path as p
+    return p.exists(file)
+
+# get os info (copied, was not written by me)
+def ex43():
+    import os, sys, platform, sysconfig
+    print("os.name                     ", os.name)
+    print("sys.platform                ", sys.platform)
+    print("platform.system()           ", platform.system())
+    print("sysconfig.get_platform()    ", sysconfig.get_platform())
+    print("platform.machine()          ", platform.machine())
+    print("platform.architecture()     ", platform.architecture())
 
